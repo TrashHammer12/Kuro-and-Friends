b0006452fcaeaaf3d0eff6bedbced336520fa586
TrashHammer12 <jmazin9142@gmail.com>
2025-09-02 15:59:20 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -2,7 +2,7 @@
 export const leagueID = "1251680367979540480"; // your league ID
 export const leagueName = "Kuro and Friends"; // your league name
 export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
